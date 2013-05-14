cde63a3210189bd8c0ee9978a0c493476b3a6554
David Grudl <david@grudl.com>
2013-05-14 16:44:18 +0200

    removed PHP 5.2 support

--- build.php
+++ build.php
@@ -16,7 +16,6 @@ use Nette\Utils\Finder;
 // configuration
 $project->gitExecutable = 'C:\Program Files (x86)\Git\bin\git.exe';
 $project->phpExecutable = realpath('tools/PHP-5.3/php.exe');
-$project->php52Executable = realpath('tools/PHP-5.2/php.exe');
 $project->apiGenExecutable = realpath('tools/ApiGen/apigen.php');
 $project->zipExecutable = realpath('tools/7zip/7z.exe');
 $project->compilerExecutable = realpath('tools/Google-Closure-Compiler/compiler.jar');
@@ -29,8 +28,6 @@ require 'tasks/latte.php';
 require 'tasks/minify.php';
 require 'tasks/minifyJs.php';
 require 'tasks/netteLoader.php';
-require 'tasks/convert52.php';
-require 'tasks/convert53.php';
 require 'tasks/php.php';
 require 'tasks/zip.php';
 
@@ -42,40 +39,38 @@ require 'tasks/zip.php';
 $project->main = function($tag = 'master', $label = '2.0') use ($project) {
 	$project->log("Building {$label}");
 
-	$dir53 = "NetteFramework-{$label}-PHP5.3";
-	$dir52p = "NetteFramework-{$label}-PHP5.2";
-	$dir52n = "NetteFramework-{$label}-PHP5.2-nonprefix";
+	$dir = "NetteFramework-{$label}";
 	$distDir = "dist/" . substr($label, 0, 3);
 
 	// export git
-	$project->delete($dir53);
-	$project->gitClone('git://github.com/nette/nette.git', $tag, $dir53);
-	$project->gitClone('git://github.com/nette/examples.git', $tag, "$dir53/examples");
-	$project->gitClone('git://github.com/nette/sandbox.git', $tag, "$dir53/sandbox");
-	$project->gitClone('git://github.com/nette/tools.git', preg_replace('#^\D*(\d+\.\d+).*\z#', 'release-$1.x', $tag), "$dir53/tools");
-	$project->gitClone('git://github.com/nette/tester.git', NULL, "$dir53/tools/Tester");
+	$project->delete($dir);
+	$project->gitClone('git://github.com/nette/nette.git', $tag, $dir);
+	$project->gitClone('git://github.com/nette/examples.git', $tag, "$dir/examples");
+	$project->gitClone('git://github.com/nette/sandbox.git', $tag, "$dir/sandbox");
+	$project->gitClone('git://github.com/nette/tools.git', preg_replace('#^\D*(\d+\.\d+).*\z#', 'release-$1.x', $tag), "$dir/tools");
+	$project->gitClone('git://github.com/nette/tester.git', NULL, "$dir/tools/Tester");
 
 	if (PHP_OS === 'WINNT') {
-		$project->exec("attrib -H $dir53\.htaccess* /s /d");
-		$project->exec("attrib -R $dir53\* /s /d");
+		$project->exec("attrib -H $dir\.htaccess* /s /d");
+		$project->exec("attrib -R $dir\* /s /d");
 	}
 
 	// create history.txt, version.txt
-	$project->git("log -n 500 --pretty=\"%cd (%h): %s\" --date-order --date=short > $dir53/history.txt", $dir53);
-	$wcrev = $project->git('log -n 1 --pretty="%h"', $dir53);
-	$wcdate = $project->git('log -n 1 --pretty="%cd" --date=short', $dir53);
-	$project->write("$dir53/version.txt", "Nette Framework $label (revision $wcrev released on $wcdate)");
+	$project->git("log -n 500 --pretty=\"%cd (%h): %s\" --date-order --date=short > $dir/history.txt", $dir);
+	$wcrev = $project->git('log -n 1 --pretty="%h"', $dir);
+	$wcdate = $project->git('log -n 1 --pretty="%cd" --date=short', $dir);
+	$project->write("$dir/version.txt", "Nette Framework $label (revision $wcrev released on $wcdate)");
 
 	// remove git files
-	foreach (Finder::findDirectories(".git")->from($dir53)->childFirst() as $file) {
+	foreach (Finder::findDirectories(".git")->from($dir)->childFirst() as $file) {
 		$project->delete($file);
 	}
-	foreach (Finder::findFiles(".git*")->from($dir53) as $file) {
+	foreach (Finder::findFiles(".git*")->from($dir) as $file) {
 		$project->delete($file);
 	}
 
 	// expand $WCREV$ and $WCDATE$
-	foreach (Finder::findFiles('*.php', '*.txt')->from($dir53)->exclude('3rdParty') as $file) {
+	foreach (Finder::findFiles('*.php', '*.txt')->from($dir)->exclude('3rdParty') as $file) {
 		$project->replace($file, array(
 			'#\$WCREV\$#' => $wcrev,
 			'#\$WCDATE\$#' => $wcdate,
@@ -83,71 +78,60 @@ $project->main = function($tag = 'master', $label = '2.0') use ($project) {
 	}
 
 	// rename *.md and delete some files
-	foreach (Finder::findFiles('*.md')->from($dir53) as $file) {
+	foreach (Finder::findFiles('*.md')->from($dir) as $file) {
 		$project->rename($file, substr($file, 0, -2) . 'txt');
 	}
-	$project->delete("$dir53/sandbox/license.txt");
-	$project->delete("$dir53/examples/license.txt");
-	$project->delete("$dir53/tools/license.txt");
-	$project->delete("$dir53/tools/Tester/license.txt");
-	$project->delete("$dir53/composer.json");
-	$project->delete("$dir53/.travis.yml");
-	$project->copy(is_file("$dir53/client-side/netteForms.js") ? "$dir53/client-side/netteForms.js" : "$dir53/client-side/forms/netteForms.js", "$dir53/sandbox/www/js/netteForms.js");
-	$project->replace("$dir53/tests/run-tests.sh", array('#(vendor|tools)/nette/tester#' => 'tools/Tester'));
-	$project->replace("$dir53/tests/RunTests.bat", array('#(vendor|tools)\\\\nette\\\\tester#' => 'tools\Tester'));
-	$project->replace("$dir53/tests/Nette/bootstrap.php", array('#(vendor|tools)/autoload.php#' => 'tools/Tester/Tester/bootstrap.php', '#Tester\\\\Helpers::setup\(\);#' => "require __DIR__ . '/../../Nette/loader.php';"));
+	$project->delete("$dir/sandbox/license.txt");
+	$project->delete("$dir/examples/license.txt");
+	$project->delete("$dir/tools/license.txt");
+	$project->delete("$dir/tools/Tester/license.txt");
+	$project->delete("$dir/composer.json");
+	$project->delete("$dir/.travis.yml");
+	$project->copy(is_file("$dir/client-side/netteForms.js") ? "$dir/client-side/netteForms.js" : "$dir/client-side/forms/netteForms.js", "$dir/sandbox/www/js/netteForms.js");
+	$project->replace("$dir/tests/run-tests.sh", array('#vendor/nette/tester#' => 'tools/Tester'));
+	$project->replace("$dir/tests/RunTests.bat", array('#vendor\\\\nette\\\\tester#' => 'tools\Tester'));
+	$project->replace("$dir/tests/Nette/bootstrap.php", array('#vendor/autoload.php#' => 'tools/Tester/Tester/bootstrap.php', '#Tester\\\\Helpers::setup\(\);#' => "require __DIR__ . '/../../Nette/loader.php';"));
 
-	// build specific packages
-	$project->delete($dir52p);
-	$project->copy($dir53, $dir52p);
-	$project->log("Building 5.2 prefixed package");
-	$project->buildPackage($dir52p, '52p');
+	// build NetteLoader.php
+	$project->netteLoader("$dir/Nette");
 
-	$project->delete($dir52n);
-	$project->copy($dir53, $dir52n);
-	$project->log("Building 5.2-nonprefix package");
-	$project->buildPackage($dir52n, '52n');
-
-	$project->log("Building 5.3 package");
-	$project->buildPackage($dir53, '53');
+	// shrink JS & CSS
+	foreach (Finder::findFiles('*.js', '*.css', '*.phtml')->from("$dir/Nette") as $file) {
+		$project->minifyJs($file);
+	}
 
 	// build minified version
-	$project->minify("$dir53/Nette", "$dir53/Nette-minified/nette.min.php", TRUE);
-	$project->minify("$dir52p/Nette", "$dir52p/Nette-minified/nette.min.php", FALSE);
-	$project->minify("$dir52n/Nette", "$dir52n/Nette-minified/nette.min.php", FALSE);
+	$project->minify("$dir/Nette", "$dir/Nette-minified/nette.min.php", TRUE);
 
 	// lint & try run PHP files
 	$project->log("Linting files");
-	$project->lint($dir53, $project->phpExecutable);
-	$project->lint($dir52p, $project->php52Executable);
-	$project->lint($dir52n, $project->php52Executable);
+	$project->php("$dir/Nette-minified/nette.min.php", $project->phpExecutable); // try run
+
+	foreach (Finder::findFiles('*.php', '*.phpt')->from($dir)->exclude('tools') as $file) {
+		if (strpos(file_get_contents($file), '@phpversion 5.4')) {
+			continue;
+		}
+		$project->phpLint($file, $project->phpExecutable);
+	}
 
 	// copy Nette to submodules
-	$project->copy("$dir53/Nette", "$dir53/sandbox/libs/Nette");
-	$project->copy("$dir52p/Nette", "$dir52p/sandbox/libs/Nette");
-	$project->copy("$dir52n/Nette", "$dir52n/sandbox/libs/Nette");
+	$project->copy("$dir/Nette", "$dir/sandbox/libs/Nette");
 
 	// build API doc
 	$apiGenConfig = dirname($project->apiGenExecutable) . '/apigen.neon';
-	$project->apiGen("$dir53/Nette", "$dir53/API-reference", $apiGenConfig, "Nette Framework $label API");
-	$project->apiGen("$dir52p/Nette", "$dir52p/API-reference", $apiGenConfig, "Nette Framework $label (for PHP 5.2, prefixed) API");
-	$project->apiGen("$dir52n/Nette", "$dir52n/API-reference", $apiGenConfig, "Nette Framework $label (for PHP 5.2, un-prefixed) API");
+	$project->apiGen("$dir/Nette", "$dir/API-reference", $apiGenConfig, "Nette Framework $label API");
 
-	$project->zip("$distDir/$dir53.zip", $dir53);
-	$project->zip("$distDir/$dir52p.zip", $dir52p);
-	$project->zip("$distDir/$dir52n.zip", $dir52n);
-	$project->zip("$distDir/$dir53.tar.bz2", $dir53);
-	$project->zip("$distDir/$dir52p.tar.bz2", $dir52p);
-	$project->zip("$distDir/$dir52n.tar.bz2", $dir52n);
+	$project->zip("$distDir/$dir.zip", $dir);
+	$project->zip("$distDir/$dir.tar.bz2", $dir);
 
 
 	// build PEAR
 	$dirPear = "Nette-$label";
 	$project->log("Building PEAR package");
 	$project->delete($dirPear);
-	$project->copy("$dir53/Nette", "$dirPear/Nette");
-	$project->copy("$dir53/readme.txt", "$dirPear/readme.txt");
-	$project->copy("$dir53/license.txt", "$dirPear/license.txt");
+	$project->copy("$dir/Nette", "$dirPear/Nette");
+	$project->copy("$dir/readme.txt", "$dirPear/readme.txt");
+	$project->copy("$dir/license.txt", "$dirPear/license.txt");
 	$project->latte("tasks/package.xml.latte", "package.xml", array(
 		'time' => time(),
 		'version' => $label,
@@ -157,40 +141,4 @@ $project->main = function($tag = 'master', $label = '2.0') use ($project) {
 
 	$project->zip("$distDir/../pear/$dirPear.tgz", array($dirPear, "package.xml"));
 	$project->delete("package.xml");
-};
-
-
-
-// supported packages: 53, 52p and 52n
-$project->buildPackage = function($dir, $package = '5.3') use ($project) {
-	if ($package !== '53') {
-		$project->delete("$dir/examples/Micro-blog");
-		$project->delete("$dir/examples/Micro-tweet");
-		$project->delete("$dir/tools/Code-Migration");
-		$project->replace("$dir/tools/Requirements-Checker/checker.php", array('#5\.3\.0#' => '5.2.0'));
-	}
-
-	foreach (Finder::findFiles('*.php', '*.phpt', '*.phpc', '*.inc', '*.phtml', '*.latte', '*.neon')->from($dir)->exclude('www/adminer', 'tools') as $file) {
-		$project->{"convert$package"}($file, TRUE);
-	}
-	$project->netteLoader("$dir/Nette");
-
-	foreach (Finder::findFiles('*.php')->from("$dir/tools/Tester", "$dir/tools/Requirements-Checker") as $file) {
-		$project->{"convert$package"}($file, TRUE);
-	}
-	// shrink JS & CSS
-	foreach (Finder::findFiles('*.js', '*.css', '*.phtml')->from("$dir/Nette") as $file) {
-		$project->minifyJs($file);
-	}
-};
-
-
-
-$project->lint = function($dir, $phpExecutable) use ($project) {
-	// try run
-	$project->php("$dir/Nette-minified/nette.min.php", $phpExecutable);
-
-	foreach (Finder::findFiles('*.php', '*.phpt')->from($dir)->exclude('tools') as $file) {
-		$project->phpLint($file, $phpExecutable);
-	}
 };
