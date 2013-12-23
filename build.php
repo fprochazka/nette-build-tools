4b20d0a0b52070bf50ea5b6024328156b923c394
David Grudl <david@grudl.com>
2013-12-23 11:41:46 +0100

    branch release-a.b.x renamed to va.b

--- build.php
+++ build.php
@@ -48,7 +48,7 @@ $project->main = function($tag = 'master', $label = '2.0') use ($project) {
 	$project->gitClone('git://github.com/nette/examples.git', $tag, "$dir/examples");
 	$project->gitClone('git://github.com/nette/sandbox.git', $tag, "$dir/sandbox");
 	$project->gitClone('git://github.com/dg/adminer-custom.git', NULL, "$dir/sandbox/vendor/dg/adminer-custom");
-	$project->gitClone('git://github.com/nette/tools.git', preg_replace('#^\D*(\d+\.\d+).*\z#', 'release-$1.x', $tag), "$dir/tools");
+	$project->gitClone('git://github.com/nette/tools.git', preg_replace('#^\D*(\d+\.\d+).*\z#', 'v$1', $tag), "$dir/tools");
 	$project->gitClone('git://github.com/nette/tester.git', NULL, "$dir/tools/Tester");
 
 	if (PHP_OS === 'WINNT') {
